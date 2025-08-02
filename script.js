09cec982fbf58342aefb1698f130ff0dc7e98fcc
Timelessthreadstshith <bharatkumawatsk473@gmail.com>
2025-08-02 16:47:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-
-function buyNow(name, price) {
-    const customer = prompt("Enter your Name:");
-    const address = prompt("Enter your Address:");
-    const pincode = prompt("Enter your Pincode:");
-    const payment = prompt("Select Payment Method: PhonePe / Google Pay / COD");
-
-    alert(`Order Placed for ${name} at ₹${price}\n\nName: ${customer}\nAddress: ${address}\nPincode: ${pincode}\nPayment: ${payment}\n\nYou will be contacted on WhatsApp.`);
-}
+// JS for auto-fill and interactivity
